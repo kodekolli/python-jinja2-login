b09a2bbd8dd74dc197748acc0cb439222351c2c4
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-03-10 16:25:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os, socket
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
@@ -28,12 +29,37 @@ def logout():
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
- if request.method == 'POST':
-  result = request.form
-  return render_template("result.html",result = result)
+    if request.method == 'POST':
+        result = request.form
+
+        app.config['MYSQL_HOST'] = 'wordpress-mysql'
+        app.config['MYSQL_USER'] = 'root'
+        app.config['MYSQL_PASSWORD'] = 'welcome'
+        app.config['MYSQL_DB'] = 'mysql'
+
+        mysql = mysql(app)
+        # Creating a connection cursor
+        cursor = mysql.connection.cursor()
+
+        cursor.execute("SHOW TABLES")
+        for x in cursor:
+            cursor.execute("CREATE TABLE students(curname VARCHAR(255), duration VARCHAR(255), interested VARCHAR(255))")
+
+        if request.method == "POST":
+            details = request.form
+            curName = details['Course Name']
+            Duration = details['Duration']
+            interested = details['Interested']
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO students(curname, duration, interested) VALUES (%s, %s, %s)",(curName, Duration, interested))
+            mysql.connection.commit()
+            cur.close()
+            return 'success'
+        return render_template("result.html",result = result)
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     host_name = socket.gethostname()
     host_ip = socket.gethostbyname(host_name)
+
     app.run(host_ip,'4000',debug=True)
